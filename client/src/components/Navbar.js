5f2fecee3b5ceedcf8a3c8ed60c68f2f827593ec
Emily Pena <emilyapena@gmail.com>
2020-03-23 16:58:45 -0600

    Nav bar styled

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider";
-import { Menu, } from 'semantic-ui-react'
+import { Menu, Segment } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
 
 class Navbar extends React.Component {
@@ -41,8 +41,8 @@ class Navbar extends React.Component {
   
   render() {
     return (
-      <div>
-        <Menu pointing secondary>
+      <Segment inverted>
+        <Menu inverted pointing secondary>
           <Link to='/'>
             <Menu.Item
               name='home'
@@ -52,7 +52,7 @@ class Navbar extends React.Component {
           </Link>
             { this.rightNavItems() }
         </Menu>
-      </div>
+      </Segment>
     )
   }
 }
