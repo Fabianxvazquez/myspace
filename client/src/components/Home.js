2b48d9e321ee71a4de9019123218deb5f73fbbdc
Emily Pena <emilyapena@gmail.com>
2020-03-24 15:52:32 -0600

    friends card showing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,63 @@
 import React from 'react';
-import { Header, } from 'semantic-ui-react';
+import axios from 'axios'
+// import { Link, } from 'react-router-dom'
+import { Header, Image, Card, Button, } from 'semantic-ui-react';
 
-const Home = () => (
-  <Header as="h3" textAlign="center">Devise Auth App</Header>
-)
+class Home extends React.Component {
+  
+  state = { friends: [], };
+  
+  componentDidMount() {
+    axios.get('/api/friends').then( res => { this.setState({ friends: res.data, })})
+  }
+
+  sample = () => {
+    const { friends, } = this.state;
+    if (friends.length) {
+      const index = Math.floor(Math.random() * friends.length);
+      return friends[index];
+    } else {
+      return null;
+    }
+  }
+  
+  render() {
+    const friend = this.sample();
+    if(friend){
+    return (
+      <div>
+      <br />
+      <Header as='h1'>Friends</Header>
+      <br />
+      <Card key={friend.id}>
+        <Card.Content>
+          <Image src={friend.avatar} />
+          <Card.Header>{friend.name}</Card.Header>
+          <Card.Meta>{friend.occupation}</Card.Meta>
+          <Card.Description>
+          {friend.gender}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='green'>
+              Approve
+            </Button>
+            <Button basic color='red'>
+              Decline
+            </Button>
+          </div>
+        </Card.Content>
+        </Card>
+        </div>
+      )
+    }
+    else {
+      return <Header textAlign="center">No More Friends</Header>
+    }
+  }
+}
+
+
 
 export default Home;
