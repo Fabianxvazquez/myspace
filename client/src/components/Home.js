3ed1710ac4938dd5d1096c96ed079fd3cc3d6d59
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-24 23:41:56 -0600

    added posts field

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Header, } from 'semantic-ui-react';
+import { Header, Form, TextArea, Button } from 'semantic-ui-react';
 
-const Home = () => (
-  <Header as="h3" textAlign="center">Your Posts</Header>
-)
+class Home extends React.Component {
+  state = { posts: '' }
 
-//
+  render (){
+    
+  return(
+    <div>
+    <Form>
+    <Header>Your Posts</Header>
+    <TextArea placeholder='Write a post ' />
+    <br />
+    <br />
+    <Button color='green' >Submit</Button>
+    <Button color='red'>Delete</Button>
+    </Form>
+    </div>
+  )
+  }
+}
+
+//UserName
+//Date of post
+//Text Form
 
 export default Home;
+
